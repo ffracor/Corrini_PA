8b5f4f1a418d0529692f76d45660e303bedd9c0b
Francesco Corrini <67370815+ffracor@users.noreply.github.com>
2023-02-02 19:53:07 +0100

    modifiche dell'ultimo minuto

---
+++ ProgettoJ/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Francesco Corrini
+ *
+ */
+module ProgettoJ {
+}
---
+++ ProgettoJ/src/partita/AVVIO.java
@@ -0,0 +1,12 @@
+package partita;
+import mostri.FactoryMostro;
+import stanze.Stanza;
+import stanze.Stanza1;
+
+public class AVVIO {
+	public static void main(String[] args) {
+		Partita partita = new Partita();
+		partita.game();
+	}
+	
+}	
